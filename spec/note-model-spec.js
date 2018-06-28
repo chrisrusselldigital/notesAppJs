1bae077f5ee3dc23f30d131fa3669bb2ef853881
Benjamin Tomkins <bentomkins@hotmail.com>
2018-06-28 22:55:22 +0100

    added first test with revealing module pattern

--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -1,7 +1,16 @@
 (function() {
 
-    var note = new Note("My favourite language is JavaScript");
+    var note = Note;
+    note.setText("My favourite language is JavaScript");
 
-    assert.isTrue(note.text === "My favourite language is JavaScript");
+    assert.isTrue(note.getText() === "<p>My favourite language is JavaScript</p>");
 
 })();
+
+// (function() {
+
+//     var note = new Note("My favourite language is JavaScript");
+
+//     assert.isTrue(note.text === "My favourite language is JavaScript");
+
+// })();
--- src/note-model.js
+++ src/note-model.js
@@ -1,11 +1,39 @@
-(function(exports) {
-  function Note(txt) {
-    this.text = txt;
-  };
+(function(win, doc,$){
+    var Note= (function(){
+            var _text;
 
-  Note.prototype.Text = function () {
-    return this.text;
-  };
+            function _makeTag(msg){
+                _text= "<p>" + msg + "</p>";
+            };
 
-  exports.Note = Note;
-})(this);
+
+        function setText(msg){
+            _makeTag(msg);
+        };
+
+        function getText(){
+            return _text;
+        };
+
+        return {
+                setText : setText,
+                getText : getText
+        };
+    })(); 
+
+    if(!win.Note) win.Note = Note;
+})(window, document,jQuery);
+
+
+
+// (function(exports) {
+//   function Note(txt) {
+//     this.text = txt;
+//   };
+
+//   Note.prototype.Text = function () {
+//     return this.text;
+//   };
+
+//   exports.Note = Note;
+// })(this);
